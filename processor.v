6b5294b01b3bfbfb00e79ffc067babf157843aca
JohnKirkvold <58152700+JohnKirkvold@users.noreply.github.com>
2019-11-24 17:25:33 -0600

    BottomHalf
    This is the bottom half from the book

--- processor.v
+++ processor.v
@@ -32,3 +32,39 @@ module proc(Data, Reset, w, Clock, F, Rx, Ry, Done, BusWires);
 	assign Gin = (I[2] | I[3]) & T[2];
 	assign Gout = (I[2] | I[3]) & T[3];
 	assign AddSub = I[3];
+	
+
+	always @(I, T, Xreg, Y)
+		for(K = 0; k < 4; k = k+1)
+		begin
+			Rin[k] =((I[0] I[1] & T[1] & Xreg[k])|
+				((I[2] I[3] & T[3] & Xreg[k]);
+			Rout[k] =(I[1] & T[1] & Y[k]) | ((I[2] | I[3]) & 
+			((T[1] & Xreg[k])| (T[2] & Y[k])));
+		end
+
+	trin tri_ext (Data, Extern, BusWires);
+	regn reg_0 (BusWires, Rin[0], Clock, R0);
+	regn reg_1 (BusWires, Rin[1], Clock, R1);
+	regn reg_2 (BusWires, Rin[2], Clock, R2);
+	regn reg_3 (BusWires, Rin[3], Clock, R3);
+
+	trin tri_0 (R0, Rout[0], BusWires);
+	trin tri_1 (R1, Rout[1], BusWires);
+	trin tri_2 (R2, Rout[2], BusWires);
+	trin tri_3 (R3, Rout[3], BusWires);
+	regn reg_A (BusWires, Ain, Clock, A);
+
+	//alu
+	always @(AddSub, A, BusWires)
+		if(!AddSub)
+			Sum = A + BusWires;
+		else 
+			Sum = A - BusWires;
+
+		regn reg_G(Sum, Gin, Clock, G);
+		trin tri_G(G, Gout, BusWires);
+
+	endmodule
+
+
