75bf50bcab377907fb217d81238c4f9da46f3197
JohnKirkvold <58152700+JohnKirkvold@users.noreply.github.com>
2019-11-24 17:53:20 -0600

    Added Trin and Regn

--- processor.v
+++ processor.v
@@ -93,4 +93,25 @@ module proc(Data, Reset, w, Clock, F, Rx, Ry, Done, BusWires);
 		assign y[3] = en & w[1] & w[0];
 
 	endmodule
-				 
+	
+	module regn(R, L, Clock, Q);
+		parameter n =8;
+		input [n-1:0]R;
+		input L, Clock;
+		output reg [n - 1:0]Q;
+
+		always @ (posedge Clock)
+			if(L)
+				Q <= R;
+
+	endmodule
+
+	module trin(Y, E, F);
+		parameter n = 8;
+		input [n-1:0]Y;
+		input E;
+		output wire [n-1:0]F;
+
+		assign F = E ? Y : 'bz;
+
+	endmodule
